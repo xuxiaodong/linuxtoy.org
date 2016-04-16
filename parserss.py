14f15471d8dde7ab4078cc10928e8ea7bfbdfc5b
Xiaodong Xu <xxdlhy@gmail.com>
2016-04-16 16:39:57 +0800

    Update templates

--- parserss.py
+++ parserss.py
@@ -93,7 +93,7 @@ html_header = """
                                     </a>
                                 </li>
 
-                                <li class="pure-menu-item">
+                                <li class="pure-menu-item menu-item-divided pure-menu-selected">
                                     <a class="pure-menu-link" href="https://linuxtoy.org/pages/comments.html" title="Comments">
                                         Comments
                                     </a>
